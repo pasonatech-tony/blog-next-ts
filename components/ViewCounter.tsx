9f17ee1c04f102b1f0a3b2a9e858bd3ead59c536
user <thoang@fab.pasona.tech>
2023-02-07 10:36:33 +0900

    add port 3000 v2

--- components/ViewCounter.tsx
+++ components/ViewCounter.tsx
@@ -7,7 +7,7 @@ async function fetcher(url) {
   return data;
 }
 
-const API_URL = process.env.API_URL || "https://blog-next-ts-hazel.vercel.app";
+const API_URL = process.env.API_URL || "http://localhost:3000";
 
 export default function ViewCounter({
   slug,
